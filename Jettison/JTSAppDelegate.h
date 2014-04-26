4bb91f63bc195ec299616d5fc505f23fadd00eac
Gabriel Rinaldi <gabriel_rinaldi@mac.com>
2014-04-26 10:32:49 -0700

    Initial Commit

---
+++ Jettison/JTSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JTSAppDelegate.h
+//  Jettison
+//
+//  Created by Gabriel Rinaldi on 4/26/14.
+//  Copyright (c) 2014 Jettison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JTSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Jettison/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Jettison
+//
+//  Created by Gabriel Rinaldi on 4/26/14.
+//  Copyright (c) 2014 Jettison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JTSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JTSAppDelegate class]));
+    }
+}
