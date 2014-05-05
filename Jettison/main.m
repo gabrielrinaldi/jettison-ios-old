693eebc1affbc9b9678614876d5ada003e9e558d
Gabriel Rinaldi <gabriel_rinaldi@mac.com>
2014-05-04 22:31:46 -0700

    Code formatting

--- Jettison/main.m
+++ Jettison/main.m
@@ -6,12 +6,11 @@
 //  Copyright (c) 2014 Jettison. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
 #import "JTSAppDelegate.h"
 
-int main(int argc, char * argv[])
-{
+#pragma mark Main
+
+int main(int argc, char * argv[]) {
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([JTSAppDelegate class]));
     }
